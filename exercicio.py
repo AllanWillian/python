4b3f3d4633fbe985d2c6d1173e91552f8753ad78
AllanWillian <allanwillian304@gmail.com>
2022-10-25 21:18:05 -0300

    exercicio1.py

---
+++ exercicio.py
@@ -0,0 +1,18 @@
+a = 5
+int(a)
+print ( "O valor da variavel é:" , a );
+
+
+b = 7.5
+float(b)
+print ( "O valor da variavel é:", b );
+
+
+c = "ola-mundo"
+print ("O que esta escrito dentro da variavel é:", c );
+
+
+d = True
+print ( "A variavel é:", d )
+
+
