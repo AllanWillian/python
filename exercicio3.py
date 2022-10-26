7ede1282288926eaacfcb3063fc9f3659df7ade7
AllanWillian <allanwillian304@gmail.com>
2022-10-25 21:59:03 -0300

    exercicio3.py

---
+++ exercicio3.py
@@ -0,0 +1,13 @@
+a = input("digite o primeiro numero")
+b = input("digite o segundo numero")
+soma = (int(a)+int(b))
+print(soma)
+
+subtracao = (int(a)-int(b))
+print (subtracao)
+
+multiplicacao = (int(a)* int(b))
+print(multiplicacao)
+
+divisao = (int(a)/ int(b))
+print(divisao)
