94daf44f78c0e269e0af10e8c3b229ddb0f4d17c
AllanWillian <allanwillian304@gmail.com>
2022-10-26 21:50:32 -0300

    26/10/2022.py

---
+++ exercicio5.py
@@ -0,0 +1,8 @@
+#if, elif, else
+n1 = int(input("Digite um numero:"))
+if n1>10:
+    print("Seu numero é maior que 10")
+elif n1<10:
+    print("Seu numero é menor que 10")
+else:
+    print("Seu numero é igual a 10")
