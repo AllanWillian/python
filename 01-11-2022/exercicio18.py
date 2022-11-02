1b0bb3cff89eabbdc785e522ce106845ee02a147
AllanWillian <allanwillian304@gmail.com>
2022-11-01 21:44:30 -0300

    01-11-2022

--- 01-11-2022/exercicio18.py
+++ 01-11-2022/exercicio18.py
@@ -5,9 +5,9 @@ penultimo = 1
 if (n == 1) or (n == 2):
     print("Número total de coelhos: 1")
 else:
-    for count in range(2, n):
+    for i in range(2, n):
         termo = ultimo + penultimo
         penultimo = ultimo
         ultimo = termo
-        count += 1
+        i += 1
     print("Número total de coelhos: ", termo)
